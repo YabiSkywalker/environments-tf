d519ce3178d617f47cc856b536d231767f3ad41c
Yabi <141942055+YabiSkywalker@users.noreply.github.com>
2025-03-29 11:43:01 -0500

    Add files via upload

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "instances" {
+ description = "EC2 config variable map"
+ type = map(object({
+   ami_id                      = string
+   instance_type               = string
+   subnet_id                   = optional(string)
+   key_name                    = string
+   vpc_security_group_ids      = optional(list(string))
+   associate_public_ip_address = bool
+   iam_instance_profile         = string
+   tags                        = map(string)
+ }))
+}
+
