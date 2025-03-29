5f92ed8e612e7bc485abc11ed70810dc70edbc30
Yabi <141942055+YabiSkywalker@users.noreply.github.com>
2025-03-29 11:42:44 -0500

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "this" {
+  for_each                      = var.instances
+  ami                           = each.value.ami_id
+  instance_type                 = each.value.instance_type
+  subnet_id                     = each.value.subnet_id
+  key_name                      = each.value.key_name
+  vpc_security_group_ids        = each.value.vpc_security_group_ids
+  associate_public_ip_address   = each.value.associate_public_ip_address
+  iam_instance_profile           = each.value.iam_instance_profile
+
+  tags = merge(
+    {
+      Name = each.key
+    },
+    each.value.tags
+ )
+}
