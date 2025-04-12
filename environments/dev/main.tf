e7b586ba2d8e36213b2e8eedd5bb6ef74c7b5c5e
Yabi <141942055+YabiSkywalker@users.noreply.github.com>
2025-04-12 08:14:06 -0500

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,13 +1,22 @@
 locals {
-  main_vpc_id    = module.umeet-vpc.vpc-id["main-vpc"]
-  security_group = module.umeet-vpc.security_group_id
-  igw_id         = module.umeet-vpc.igw_id
-  route-table-id = module.umeet-vpc.route-table-id
-  subnet-id      = module.umeet-vpc.subnet_id
+/*
+  main_vpc_id                     = module.umeet-vpc.vpc-id["main-vpc"]
+  security_group                  = module.umeet-vpc.security_group_id
+  igw_id                          = module.umeet-vpc.igw_id
+  route-table-id                  = module.umeet-vpc.route-table-id
+  subnet-id                       = module.umeet-vpc.subnet_id
+*/
+  google-proj-svc-name            = module.google-apigee.google-project-service-project
+  google-proj-svc-id              = module.google-apigee.google-project-service-id
+  google-apigee-org-id            = module.google-apigee.google-apigee-organization-id
+  google-apigee-environment-name  = module.google-apigee.google-apigee-environment-name
+  google-apigee-instance-id       = module.google-apigee.google-apigee-instance-id
+  google-compute-network-id       = module.google-apigee.google-compute-network-id
 }
 
+/*
 module "umeet-vpc" {
-  source = "../../modules/network"
+  source = "../../modules/AmazonWebServices/network"
 
   vpc                 = {
     main-vpc = {
@@ -156,7 +165,7 @@ module "umeet-vpc" {
 }
 
 module "umeet_api_role" {
-  source = "../../modules/iam"
+  source = "../../modules/AmazonWebServices/iam"
 
   iam_role_name = "umeet_role_dev"
   iam_role_policy = jsonencode({
@@ -199,7 +208,7 @@ module "umeet_api_role" {
 }
 
 module "umeet_bucket" {
-  source                        = "../../modules/s3"
+  source                        = "../../modules/AmazonWebServices/s3"
 
   bucket                        = "umeet-dev-bucket"
   force_destroy                 = true
@@ -215,7 +224,7 @@ module "umeet_bucket" {
 }
 
 module "umeet_instances" {
- source = "../../modules/ec2"
+ source = "../../modules/AmazonWebServices/ec2"
 
  instances = {
    web-1 = {
@@ -251,7 +260,7 @@ module "umeet_instances" {
 }
 
 module "route53_record" {
-  source = "../../modules/route53"
+  source = "../../modules/AmazonWebServices/route53"
 
   dns_record = {
     linkd-dev = {
@@ -260,6 +269,59 @@ module "route53_record" {
       type    = "A"
       ttl     = 1
       records = [module.umeet_instances.public-ip["web-1"]]
+    }
+  }
+}
+*/
+
+/* ------------------ GOOGLE APIGEE ------------------ */
+data "google_client_config" "current" {}
+
+module "google-apigee" {
+  source = "../../modules/GoogleCloudPlatform/APIGEE"
+
+  google-project-service     = {
+    umeet-project-service = {
+        project = "umeet-gate"
+        service = "apigee.googleapis.com"
+    }
+  }
+
+  google-compute-network = {
+    umeet-api-gateway-vpc = {
+      project   = local.google-proj-svc-name["umeet-project-service"]
+      name      = "umeet-api-gateway-network"
+    }
+  }
+
+  google-apigee-organization = {
+    umeet-apigee-org = {
+      project_id         = data.google_client_config.current.project
+      //local.google-proj-svc-id["umeet-project-service"]
+      analytics_region   = "us-central1"
+      authorized_network = local.google-compute-network-id["umeet-api-gateway-vpc"]
+    }
+  }
+
+  google-apigee-environment = {
+    umeet-apigee-env = {
+      org_id = local.google-apigee-org-id["umeet-apigee-org"]
+      name   = "umeet-dev"
+    }
+  }
+
+  google-apigee-instance     = {
+    umeet-apigee-instance = {
+        name             = "umeet-dev-apigee-instance"
+        org_id           = local.google-apigee-org-id["umeet-apigee-org"]
+        location         = "us-central1"
+        #runtime_ip_range = "10.138.0.0/20"
+    }
+  }
+  google-apigee-environment-attachment = {
+    umeet-apigee-environment-attachment = {
+        environment = local.google-apigee-environment-name["umeet-apigee-env"]
+        instance_id = local.google-apigee-instance-id["umeet-apigee-instance"]
     }
   }
 }
